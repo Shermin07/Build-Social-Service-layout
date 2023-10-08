4c8bdb37f7482d819b769c99bcc07b7dc3d3bd95
Shermin Sultana <rima64549@gmail.com>
2023-10-09 04:51:08 +0600

    update firebase config

--- src/Firebase/Firebase.init.js
+++ src/Firebase/Firebase.init.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAslDKRmXRZvqCAnTGXsGdFrXG2PSD6KAY",
-  authDomain: "a9-build-social-service-layout.firebaseapp.com",
-  projectId: "a9-build-social-service-layout",
-  storageBucket: "a9-build-social-service-layout.appspot.com",
-  messagingSenderId: "737826987629",
-  appId: "1:737826987629:web:0c7b511b417de0e8a06964"
+  apiKey:import.meta.env.VITE_apiKey,
+  authDomain:import.meta.env.VITE_authDomain ,
+  projectId:import.meta.env.VITE_projectId ,
+  storageBucket:import.meta.env.VITE_storageBucket ,
+  messagingSenderId:import.meta.env.VITE_messagingSenderId ,
+  appId:import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
